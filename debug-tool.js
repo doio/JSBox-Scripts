0f334f855638b03ecb68590fffe7a830ec9f1e12
Eva1ent <24217275+186c0@users.noreply.github.com>
2018-02-26 22:30:21 +0800

    Add files via upload

---
+++ debug-tool.js
@@ -0,0 +1,50 @@
+// 注意:本拓展运行期间可能会造成JSBox不稳定
+
+function debug() {
+  let json = {
+    timestamp: new Date().toISOString(),
+    views: keyWindow.invoke("recursiveDescription").rawValue().toString(),
+    viewControllers: rootVC.invoke("_printHierarchy").rawValue().toString()
+  };
+  $addin.save({
+    name: "tmp.js",
+    data: $data({
+      string: JSON.stringify(json)
+    })
+  });
+  $ui.toast("running...");
+}
+
+let UIApp = $objc("UIApplication"),
+  SApp = UIApp.invoke("sharedApplication"),
+  keyWindow = SApp.invoke("keyWindow"),
+  rootVC = keyWindow.invoke("rootViewController");
+let timer;
+let Options = [{
+    name: 'START',
+    func: start
+  },
+  {
+    name: 'STOP',
+    func: stop
+  }
+];
+
+function start() {
+  timer = $timer.schedule({
+    interval: 0.5,
+    handler: debug
+  });
+}
+
+function stop() {
+  timer.invalidate();
+  $ui.toast("stopped");
+}
+$ui.menu({
+  items: Options.map(i => i.name),
+  handler: (e, i) => {
+    mode = 'more';
+    Options[i].func();
+  }
+});
